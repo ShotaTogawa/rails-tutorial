eaafbd471b2a0680a8c107e69e0cc130757592ce
ShotaTogawa <s.togawa0112@gmail.com>
2020-04-17 11:19:40 +0900

    finished sec 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,23 +11,31 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence:true, length: { minimum: 6 }
 
-  # class << selfを使ってdigestとnew_tokenメソッドを定義する
-  class << self
-  # 渡された文字列のハッシュ値を返す
-  def digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # ランダムなトークンを返す
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  # 永続セッションのためにユーザーをデータベースに記憶する
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
+    # class << selfを使ってdigestとnew_tokenメソッドを定義する
+    # 渡された文字列のハッシュ値を返す
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+  
+    def remember
+      self.remember_token = User.new_token
+      self.update_attribute(:remember_digest,
+        User.digest(remember_token))
+    end
+  
+    def forget
+      self.update_attribute(:remember_digest, nil)
+    end
+  
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
+    end
 end
